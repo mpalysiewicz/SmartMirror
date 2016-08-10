1b51992f51c19376ed31b126ad050206843ce29e
Greg <swieczkowski@gmail.com>
2016-08-11 00:06:59 +0200

    Initial version of: 1) Universal App client 2) Motion sensor service

---
+++ Win_IoT/Motion sensor/MotionSensor.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Gpio;
+
+namespace MotionSensorService
+{
+    public sealed class MotionSensor
+    {
+        GpioPin motionSensorPin;
+        const int Pin_GPIO17 = 11;
+
+        public void InitGPIO()
+        {
+            var gpio = GpioController.GetDefault();
+                        
+            if (gpio == null)
+            {
+                //There is no GPIO controller on this device.
+                motionSensorPin = null;                
+                return;
+            }
+
+            motionSensorPin = gpio.OpenPin(Pin_GPIO17);
+                        
+            if (motionSensorPin == null)
+            {
+                //There were problems initializing the GPIO pin.;
+                return;
+            }
+
+            motionSensorPin.SetDriveMode(GpioPinDriveMode.Input);
+            
+            motionSensorPin.ValueChanged += MotionSensorPin_ValueChanged;
+            //GPIO pin initialized correctly.
+        }
+
+        private void MotionSensorPin_ValueChanged(GpioPin sender, GpioPinValueChangedEventArgs args)
+        {
+            if (args.Edge == GpioPinEdge.RisingEdge)
+
+                MotionDetected.Invoke(this, null);
+        }
+
+        public event Windows.Foundation.TypedEventHandler<MotionSensor,string> MotionDetected;
+
+        public string Read()
+        {
+            if (motionSensorPin == null)
+                return string.Empty;
+            return motionSensorPin.Read().ToString();
+        }
+    }
+}
---
+++ Win_IoT/WPF Client/MainPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using MotionSensorService;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace MotionSensor
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        //private DispatcherTimer timer;
+        private MotionSensorService.MotionSensor motionSensor;
+        private DispatcherTimer timer;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            motionSensor = new MotionSensorService.MotionSensor();
+            motionSensor.InitGPIO();
+            motionSensor.MotionDetected += MotionSensor_MotionDetected;
+
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromMilliseconds(500);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+
+
+        }
+
+        private void Timer_Tick(object sender, object e)
+        {
+            var value = motionSensor.Read();
+            if (value == "High")
+                GpioStatus.Items.Add("Motion detected at " + DateTime.Now);
+        }
+
+        private void MotionSensor_MotionDetected(MotionSensorService.MotionSensor sender, string e)
+        {
+            //GpioStatus.Text += "\r\nMotion detected at " + DateTime.Now;
+        }
+        
+    }
+}
