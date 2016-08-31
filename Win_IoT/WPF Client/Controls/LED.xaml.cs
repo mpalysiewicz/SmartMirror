6a1d4c8a3c578cb098e9cb51f1366f10c68accd1
Greg <swieczkowski@gmail.com>
2016-08-31 22:22:38 +0200

    LED fixes

--- Win_IoT/WPF Client/Controls/LED.xaml.cs
+++ Win_IoT/WPF Client/Controls/LED.xaml.cs
@@ -6,21 +6,33 @@ namespace ABB.MagicMirror.Controls
     public sealed partial class LED : UserControl
     {
         public LED()
-        {
+        {            
             InitializeComponent();
+            Color = Color.FromArgb(255, 153, 0, 0);
             Off();
         }        
 
+        /// <summary>
+        /// Defines LED color.
+        /// </summary>
         public Color Color { get; set; }
+
+        /// <summary>
+        /// Indicates if LED is turned on.
+        /// </summary>
+        public bool IsTurnedOn { get; private set; }
+
 
         public void On()
         {
-            Color = Color.FromArgb(255, 153, 0, 0);
+            LedColor.Color = Color;
+            IsTurnedOn = true;
         }
 
         public void Off()
         {
-            Color = Color.FromArgb(255, 0, 0, 0);
+            LedColor.Color = Color.FromArgb(255, 0, 0, 0);
+            IsTurnedOn = false;
         }
     }
 }
