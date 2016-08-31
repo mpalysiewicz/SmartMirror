b41418efc7b8bc575d43ea07002cb33e25d336d5
Greg <swieczkowski@gmail.com>
2016-08-31 22:56:45 +0200

    Temperature and humidity component extracted for building GUI from blocks

---
+++ Win_IoT/WPF Client/GuiComponents/TemperatureAndHumidityComponent.xaml.cs
@@ -0,0 +1,34 @@
+﻿using ABB.Sensors.TemperatureWrapper;
+using System;
+using Windows.UI.Core;
+using Windows.UI.Xaml.Controls;
+
+namespace ABB.MagicMirror.GuiComponents
+{
+    public sealed partial class TemperatureAndHumidityComponent : UserControl
+    {
+        private TemperatureSensor temperatureSensor;
+
+        public TemperatureAndHumidityComponent()
+        {
+            this.InitializeComponent();
+            SetupTemperatureSensor();
+        }
+        
+        private void SetupTemperatureSensor()
+        {
+            temperatureSensor = new TemperatureSensor(10000);
+            if (temperatureSensor == null)
+                return;
+            temperatureSensor.TemperatureRead += TemperatureSensor_TemperatureRead;
+        }
+        private async void TemperatureSensor_TemperatureRead(object sender, TemperatureReadingArgs e)
+        {
+            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                TemperatureValue.Text = e.Temperature + "°C";
+                HumidiyValue.Text = e.Humidity + "%";
+            });
+        }
+    }
+}
--- Win_IoT/WPF Client/MainPage.xaml.cs
+++ Win_IoT/WPF Client/MainPage.xaml.cs
@@ -1,16 +1,13 @@
 ﻿using System;
 using Windows.UI.Xaml.Controls;
-using ABB.Sensors.Motion;
 using Windows.UI.Core;
 using Windows.UI.Xaml;
 using ABB.Sensors.Distance;
-using ABB.Sensors.TemperatureWrapper;
 
 namespace ABB.MagicMirror
 {
     public sealed partial class MainPage : Page
-    {
-        private TemperatureSensor temperatureSensor;
+    {        
         private DistanceSensorHCSR04 _distanceSensor;
         private DispatcherTimer timer;
 
@@ -19,9 +16,7 @@ namespace ABB.MagicMirror
             this.InitializeComponent();
 
             ShowIpAddress();
-
-            temperatureSensor = new TemperatureSensor(10000);
-            temperatureSensor.TemperatureRead += TemperatureSensor_TemperatureRead;
+                                    
             timer = new DispatcherTimer();
             timer.Interval = TimeSpan.FromMilliseconds(100);
             timer.Tick += Timer_Tick;
@@ -30,7 +25,7 @@ namespace ABB.MagicMirror
             _distanceSensor = new DistanceSensorHCSR04();
             _distanceSensor.InitGPIO();
         }
-
+                
         private void ShowIpAddress()
         {
             ipAddressTbx.Text = string.Join(",  ", Helpers.Networking.GetLocalIpAddress());
@@ -44,13 +39,6 @@ namespace ABB.MagicMirror
             });
         }
         
-        private async void TemperatureSensor_TemperatureRead(object sender, TemperatureReadingArgs e)
-        {
-            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
-            {
-                TemperatureValue.Text = e.Temperature + "°C";
-                HumidiyValue.Text = e.Humidity + "%";
-            });
-        }
+       
     }
 }
