bdeb16bacafd3baeb5aac44b7c3cbc44a16c4040
DarkJarek <darkjarek@gmail.com>
2016-09-19 12:28:34 +0200

    Saving sensors data to MongoDB - in development

--- SensorsService/SensorsService/server.js
+++ SensorsService/SensorsService/server.js
@@ -5,6 +5,10 @@ var bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
 
+var mongodb = require('mongodb');
+var mongoClient = mongodb.MongoClient;
+var url = 'mongodb://10.3.54.74:27017/sensors';
+
 //GET all data
 app.get('/listSensorsData', function (req, res) {
     fs.readFile(__dirname + "/" + "sensorsData.json", 'utf8', function (err, data) {
@@ -37,6 +41,34 @@ app.get('/:id', function (req, res) {
 app.post('/save', function (req, res) {
     console.log(req.body);
     fs.appendFileSync(__dirname + "/" + "sensorsData.json", req.body, 'utf-8');
+
+    mongoClient.connect(url, function (err, db) {
+        if (err) {
+            console.log('Unable to connect to the mongoDB server. Error:', err);
+        } else {
+            //HURRAY!! We are connected. :)
+            console.log('Connection established to', url);
+
+            // Get the documents collection
+            var collection = db.collection('users');
+
+            //Create some users
+            var user1 = {name: 'modulus admin', age: 42, roles: ['admin', 'moderator', 'user']};
+            var user2 = {name: 'modulus user', age: 22, roles: ['user']};
+            var user3 = {name: 'modulus super admin', age: 92, roles: ['super-admin', 'admin', 'moderator', 'user']};
+
+            // Insert some users
+            collection.insert([user1, user2, user3], function (err, result) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log('Inserted %d documents into the "users" collection. The documents inserted with "_id" are:', result.length, result);
+                }
+                //Close connection
+                db.close();
+            });
+        }
+    });
 });
 
 
