9d75a97d31a8504663aebd3e0d015393d7cdb1a1
Mateusz <Mateusz@DESKTOP-A1H26DT>
2016-09-19 09:57:33 +0200

    Added distance sensor service

---
+++ Win_IoT/DistanceSensorService/DistanceSensorReader.cs
@@ -0,0 +1,40 @@
+﻿using ABB.Sensors.Distance;
+using SensorDataForwarder;
+using System.Threading;
+
+namespace DistanceSensorService
+{
+    class DistanceSensorReader
+    {
+        private DistanceSensorHCSR04 _distanceSensor;
+        private Timer timer;
+        private SensorDataSender sensorDataSender;
+
+        public DistanceSensorReader(string url)
+        {
+            sensorDataSender = new SensorDataSender(url);
+
+            _distanceSensor = new DistanceSensorHCSR04();
+            _distanceSensor.InitGPIO();
+
+            InitTimer(100);
+        }
+
+
+
+        private void InitTimer(int readRate)
+        {
+            timer = new Timer(ReadDistance, null, 0, readRate);
+        }
+
+        private void ReadDistance(object state)
+        {
+            var distanceReading = _distanceSensor.Read();
+            if (distanceReading == null)
+            {
+                return;
+            }
+            sensorDataSender.SendObjectAsJson(distanceReading);
+        }
+    }
+}
